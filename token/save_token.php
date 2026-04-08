a68755ad33e545615a75d5786355d90ab5f065ac
nidsys <nidsys@yahoo.co.kr>
2026-04-08 16:55:08 +0900

    1

--- token/save_token.php
+++ token/save_token.php
@@ -1,5 +1,5 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-<script src="../assets/js/app.js"></script>
+<script src="../assets/js/randkey.js"></script>
 
 <script>
 // 앱 실행 후 푸시 토큰 받으면 서버로 전송
